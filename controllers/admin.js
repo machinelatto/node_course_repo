2075489de8838d0848217c9d0194620359383d00
Mateus Chinelatto <machinelatto@gmail.com>
2020-04-08 17:28:57 -0300

    Aula 124, falta ainda atualizar o parametro value no forms com a logica para preencher sozinho ou nao

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,6 +6,7 @@ exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product',{
       pageTitle: 'Add Product',
       path: '/admin/add-product',
+      editing: false
     })
 }
 
@@ -25,12 +26,22 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
     const editMode = req.query.edit
-    
-    res.render('admin/edit-product',{
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode
+    if(!editMode){
+      return res.redirect('/')
+    }
+    const prodId = req.params.productId
+    Product.findById(prodId,product => {
+      if(!product){
+        return res.redirect('/')
+      }
+      res.render('admin/edit-product',{
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      })
     })
+
 }
 
 
