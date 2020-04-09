9c6a2cb6dfd936a8a7c596d648c23259633b5a75
Mateus Chinelatto <machinelatto@gmail.com>
2020-04-09 10:39:20 -0300

    Pgina de edição adicionada, bem como redirecionamento após a edição, proximo passo é poder deletar algo do carrinho, aula 127

--- controllers/admin.js
+++ controllers/admin.js
@@ -16,7 +16,7 @@ exports.postAddProduct = (req, res, next) => {
     const imageUrl = req.body.imageUrl
     const description = req.body.description
     
-    const product = new Product(title, imageUrl, description, price)
+    const product = new Product(null, title, imageUrl, description, price)
     
     product.save()
     // quando recebe uma requisição POST para /add-product redireciona para a raiz
@@ -34,6 +34,7 @@ exports.getEditProduct = (req, res, next) => {
       if(!product){
         return res.redirect('/')
       }
+      console.log(product)
       res.render('admin/edit-product',{
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
@@ -44,7 +45,16 @@ exports.getEditProduct = (req, res, next) => {
 
 }
 
-
+exports.postEditProduct = (req,res,next) =>{
+  const prodId = req.body.productId
+  const updatedTitle = req.body.title
+  const updatedPrice = req.body.price
+  const updatedImageUrl = req.body.imageUrl
+  const updatedDesc = req.body.description
+  const updatedProduct = new Product(prodId,updatedTitle, updatedImageUrl, updatedDesc, updatedPrice)
+  updatedProduct.save()
+  res.redirect('/admin/products')
+}
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products)=> {
