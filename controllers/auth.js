1e10143349e2dfdced9a679ba9f0bd3500ff151b
Mateus Chinelatto <machinelatto@gmail.com>
2020-05-22 15:08:17 -0300

    Adicionadas rotas e views para mudar senha, também adicionado envio de emails

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,21 @@
+const crypto = require('crypto')
+// Name	Blanca Dickens
+// Username	blanca11@ethereal.email (also works as a real inbound email address)
+// Password	D8Cdn3vqrszFcqPDRZ
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer')
 
 const User = require('../models/user');
+
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  auth: {
+      user: 'blanca11@ethereal.email',
+      pass: 'D8Cdn3vqrszFcqPDRZ'
+  }
+});
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -39,7 +54,7 @@ exports.postLogin = (req, res, next) => {
       }
       res.redirect('/login')
     })
-    .cathc(err=>{
+    .catch(err=>{
       console.log(err);
       res.redirect('/login')
     })
@@ -67,16 +82,66 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result =>{
           res.redirect('/login')
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@node-complete.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!<h1>'
+          })
+        })
+        .catch(err => {
+          console.log(err)
         })
     })
     .catch(err=>{
-      console.log()
+      console.log(err)
     });
 };
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
-    console.log(err);
+    //console.log(err);
     res.redirect('/');
+  });
+};
+
+exports.getReset = (req,res,next) => {
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: req.flash('error')
+  });
+}
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err,buffer)=>{
+    if(err){
+      console.log(err)
+      return res.redirect('/reset')
+    }
+    const token = buffer.toString('hex')
+    User.findOne({email:req.body.email})
+      .then(user=>{
+        if(!user){
+          req.flash('error', 'No account with that email found')
+          return res.redirect('/reset')
+        }
+        user.resetToken = token
+        user.resetTokenExpiration = Date.now() + 3600000
+        return user.save()
+      })
+      .then(result=>{
+        res.redirect('/')
+        transporter.sendMail({
+          to: req.body.email,
+          from: 'shop@node-complete.com',
+          subject: 'Password reset',
+          html: `
+            <p>You requested a password reset</p>
+            <p>Click this <a href="http://localhost:8080/reset/${token}">link</a> to set a new password</p>
+          `
+        })
+      })
+      .catch(err=>{console.log(err)})
   });
 };
